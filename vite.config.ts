f2987c7f9d18588263d35e18fd314e150d53946d
victor Alomia <alomia18@gmail.com>
2025-03-31 22:33:25 -0500

    configurando tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
+ import { defineConfig } from 'vite'
+ import react from '@vitejs/plugin-react-swc'
+ import tailwindcss from '@tailwindcss/vite'
+ // https://vite.dev/config/
+ export default defineConfig({
+   plugins: [react(),
+     tailwindcss(),
+   ],
+ })
 // import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-// //import tailwindcss from '@tailwindcss/vite'
-// // https://vite.dev/config/
+// import tailwindcss from '@tailwindcss/vite'
 // export default defineConfig({
-//   plugins: [react(),
-//     //tailwindcss(),
+//   plugins: [
+//     tailwindcss(),
 //   ],
 // })
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
